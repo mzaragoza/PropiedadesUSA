23700d11130a8cd3b6da8e1a0ad1d3de1be274a8
Moises Zaragoza <mzaragoza@circletechfl.com>
2024-09-09 19:34:04 -0500

    localizazion

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
+
+  def set_locale
+    if params[:locale]
+      session[:locale] = params[:locale]
+    end
+    I18n.locale = session[:locale] || I18n.default_locale
+  end
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
+
 end
