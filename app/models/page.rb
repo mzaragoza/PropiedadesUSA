fe754f9dbcdc558f16450484cfd1ac712bc4b8ba
Moises Zaragoza <mzaragoza@circletechfl.com>
2024-09-27 11:56:44 -0500

    portfolio

--- app/models/page.rb
+++ app/models/page.rb
@@ -15,4 +15,19 @@ class Page
   def self.total_investment_value
     (((Date.today.year + Date.today.yday) * 5.7) * 70000.8)
   end
+
+  def self.portfolio
+    [
+      {image: '/assets/public/assets/img/masonry-portfolio/single-family1.png', kind: 'single-family'},
+      {image: '/assets/public/assets/img/masonry-portfolio/single-family2.png', kind: 'single-family'},
+      {image: '/assets/public/assets/img/masonry-portfolio/single-family3.png', kind: 'single-family'},
+      {image: '/assets/public/assets/img/masonry-portfolio/single-family4.png', kind: 'single-family'},
+      {image: '/assets/public/assets/img/masonry-portfolio/single-family5.png', kind: 'single-family'},
+      {image: '/assets/public/assets/img/masonry-portfolio/multifamily1.png', kind: 'multifamily'},
+      {image: '/assets/public/assets/img/masonry-portfolio/multifamily2.png', kind: 'multifamily'},
+      {image: '/assets/public/assets/img/masonry-portfolio/multifamily3.png', kind: 'multifamily'},
+      {image: '/assets/public/assets/img/masonry-portfolio/multifamily4.png', kind: 'multifamily'},
+      {image: '/assets/public/assets/img/masonry-portfolio/multifamily5.png', kind: 'multifamily'},
+    ]
+  end
 end
