9406252e4a115d9bfd1def0143bc62767be4be3e
Moises Zaragoza <mzaragoza@circletechfl.com>
2024-09-30 00:18:13 -0500

    test email mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,14 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "propiedadesusa@approvemylease.com"
   layout "mailer"
+
+  # ApplicationMailer.test.deliver_now
+  def test
+    email = mail(
+        from: "propiedadesusa@leasetaurant.com",
+        to:      'propiedadesusa@moisesZaragoza.com',
+        subject: 'This is a test from Propiedades USA',
+        body: "This is a test from Propiedades USA #{DateTime.now}"
+    )
+  end
 end
