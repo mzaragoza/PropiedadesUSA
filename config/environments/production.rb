508e672953cd0b116eba8fff68a8c28e9e2b5dd5
Moises Zaragoza <mzaragoza@circletechfl.com>
2024-09-30 00:28:33 -0500

    test email mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,6 +75,12 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { :host => 'propiedadesusa-512fc3df5fee.herokuapp.com/' }
   config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :sendgrid_actionmailer
+  config.action_mailer.sendgrid_actionmailer_settings = {
+    api_key: ENV['SENDGRID_API_KEY'],
+    raise_delivery_errors: true
+  }
+
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
